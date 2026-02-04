5c50efaa708f58627139b86213cf55e99cf810f2
pezimbron <102393021+pezimbron@users.noreply.github.com>
2026-02-03 19:38:21 -0600

    migration fix

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20241125_222020_initial from './20241125_222020_initial';
 import * as migration_20241214_124128 from './20241214_124128';
 import * as migration_20260115_add_request_status from './20260115_add_request_status';
 import * as migration_20260115_add_workflow_fields from './20260115_add_workflow_fields';
+import * as migration_20260203_add_scheduling_and_qb_fields from './20260203_add_scheduling_and_qb_fields';
 
 export const migrations = [
   {
@@ -23,5 +24,10 @@ export const migrations = [
     up: migration_20260115_add_workflow_fields.up,
     down: migration_20260115_add_workflow_fields.down,
     name: '20260115_add_workflow_fields',
+  },
+  {
+    up: migration_20260203_add_scheduling_and_qb_fields.up,
+    down: migration_20260203_add_scheduling_and_qb_fields.down,
+    name: '20260203_add_scheduling_and_qb_fields',
   },
 ];
